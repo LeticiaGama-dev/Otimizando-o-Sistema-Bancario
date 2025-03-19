bbda441707f9cfdc7dbb1701477c622edc147c45
LeticiaGama-dev <dev.leticiacode@gmail.com>
2025-03-19 09:21:39 -0300

    Update Otimizando o Sistema Bancário com Funções Python.py

--- Otimizando o Sistema Bancário com Funções Python.py
+++ Otimizando o Sistema Bancário com Funções Python.py
@@ -1,13 +1,54 @@
+def depositar(saldo,extrato):
+    deposito = int(input('Digite o valor a ser depositado: R$ '))
+    if deposito >0:
+        saldo+= deposito
+        extrato += f'Movimento do tipo DEPÓSITO no valor de R$ {deposito:.2f}\n'
+        print(f'O valor depositado nesta transação foi de R${deposito:.2f}')
+    return saldo,extrato
+
+def sacar(saldo, extrato, numero_saques,limite,LIMITE_SAQUES):
+    saque = int(input('Digite o valor a ser sacado: R$ '))
+
+    if saque > limite :
+        print('O valor é superior ao máximo permitido.Valor limite por saque = R$ 500,00')
+
+    elif saque > saldo:
+        print('Saldo insuficiente!')
+
+    elif numero_saques > LIMITE_SAQUES:
+        print('Número de saques excedido! Limite diário, 3 saques!')
+
+    elif numero_saques == LIMITE_SAQUES:
+        print('ATENÇÃO: Número de saques diários atingidos!')
+
+    else:
+        saldo-= saque
+        extrato += f'Movimentação do tipo SAQUE no valor de R${saque:.2f}\n'
+        numero_saques += 1
+        print(f'O valor sacado nesta transação foi de R${saque:.2f}')
+
+    return saldo, extrato, numero_saques
+
+def exibir_extrato(extrato):
+    print('-' * 30, 'EXTRATO', '-' * 30)
+    print(f'{extrato:^70}')
+    print(f'{extrato if extrato else 'Nenhuma movimentação realizada.':^70}')
+    print('-' * 69)
+
+
 print('-='*30)
 print(F'{'BANCO DIO':^60}')
 print('-='*30)
 print('Bem-vindo ao Banco Dio.Selecione a opção desejada:')
+
 menu = """
 [D] Depositar
 [S] Sacar
 [E] Extrato
 [Q] Sair
  """
+
+#definição das variáveis 
 saldo = 1800
 limite = 500
 extrato = ""
@@ -19,54 +60,33 @@ while True:
 
     opcao = input(menu).strip().upper()
 
-    if opcao not in 'DSEQ':
-        print('Opção inválida, selecione uma das opções do menu abaixo:')
-       
-
     if opcao == 'D': #Depósito
-        deposito = int(input('Digite o valor a ser depositado. R$:'))
+        saldo, extrato = depositar(saldo,extrato)
+           
+    elif opcao == 'E': #Extrato
+       # print('-' * 30, 'EXTRATO', '-' * 30)
+        #print(f'{extrato:^70}')
+        #print('-' * 69)
+        print(extrato)
+    else:
+        print(f'{extrato if extrato else 'Nenhuma movimentação realizada.':^70}')
 
-        if deposito > 0:
-            saldo += deposito
-            extrato += f'Movimentação do tipo DEPÓSITO no valor de R${deposito:.2f}\n'
-            print(f'O valor depositado nesta transação foi de R${deposito:.2f}')
+    elif opcao == 'S': #Saque
+        saldo,extrato,numero_saques = sacar(saldo,extrato,numero_saques,limite,LIMITE_SAQUES)
 
-    if opcao == 'Q': #Sair
+    elif opcao == 'Q': #Sair
         print('-'*60)
         print(f'{'Agradecemos por utilizar o Banco Dio. Até breve!':^60}')
         print('-' * 60)
         break
+    
+    else:
+        print('Opção inválida, selecione uma das opções do menu abaixo:')
 
-    if opcao == 'E': #Extrato
-        print('-' * 30, 'EXTRATO', '-' * 30)
-        print(f'{extrato:^70}')
-        print(f'{extrato if extrato else 'Nenhuma movimentação realizada.':^70}')
-        print('-' * 69)
-
-    if opcao == 'S': #Saque
-        saque = int(input('Digite o valor a ser sacado: R$'))
-
-        if saque > limite:
-            print('O valor é superior ao máximo permitido. Valor limite por saque = R$500.00')
-
-        elif saque <= saldo and numero_saques <=3 and numero_saques < 3 :
-            numero_saques +=1
-            saldo -= saque
-            extrato += f'Movimentação do tipo SAQUE no valor de R${saque:.2f},\n'
-            print(f'O valor sacado nesta transação foi de R${saque:.2f}')
-            print(f'O número de saques diário é de: {numero_saques}')
-
-        if numero_saques > 3:
-            print('Número de saques excedido!')
-
-        elif numero_saques == 3:
-            print('Limite diário de 03 saques atingido!')
-
-        elif saque > saldo:
-            print('Saldo insuficiente!')
-
-
-
-
+        
+    
+    
+    
+         
 
 
